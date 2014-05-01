0a993dc51f57617c27527e47499096e58554e7e9
Blaine Bublitz <blaine@iceddev.com>
2014-04-30 20:03:28 -0700

    add demo page

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 'use strict';
 
 var scrape = require('./lib/scrape');
+var geocode = require('./lib/geocode');
 
 var Hapi = require('hapi');
 
 var server = new Hapi.Server('0.0.0.0', 3000, {
-  cors: true
+  cors: true,
+  debug: {
+    request: ['error']
+  }
 });
 
 server.route({
   method: 'GET',
   path: '/',
   handler: function(request, reply){
-    scrape().then(reply);
+    scrape()
+      .then(geocode)
+      .then(reply)
+      .otherwise(reply);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/{path*}',
+  handler: {
+    directory: {
+      path: __dirname + '/public'
+    }
   }
 });
 
